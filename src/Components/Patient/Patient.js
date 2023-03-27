d60de2dc116fb3803c4e77de38d32b532c898f21
Zia <ziaualhassain@gmail.com>
2023-03-27 12:08:47 +0530

    Added where the doctor can see the patient timeline
    Added the component where the doctor can see the timeline of the patient but cannot see the recent approvals

--- src/Components/Patient/Patient.js
+++ src/Components/Patient/Patient.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { toast } from "react-toastify";
 import "./Patient.css";
 
 function PatientComponent() {
 
   const patientAddress = "0x12345678901234......567890"; //Address of the patient
+
+  const authorizedAddress = 'patientAddress'; //making only the patient can access some key features such as toggling recent approvals
 
   const recentApprovals = [
     "0x123456.......890abcdef12345678",
@@ -24,52 +27,56 @@ function PatientComponent() {
       title: "Annual Physical Exam",
       description: "Routine checkup with primary care physician",
     },
-
     {
-      date: "November 2021",
-      title: "Emergency Room Visit",
-      description:
-        "Admitted for chest pain; discharged after tests showed no major issues",
-    },  
-
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
     {
-        date: "Somemonth 2022",
-        title: "Annual Physical Exam",
-        description: "Routine checkup with primary care physician",
-      },
-
-      {
-        date: "Somemonth 2021",
-        title: "Emergency Room Visit",
-        description:
-          "Admitted for chest pain; discharged after tests showed no major issues",
-      },   
-
-      {
-        date: "Somemonth 2022",
-        title: "Annual Physical Exam",
-        description: "Routine checkup with primary care physician",
-      },
-
-      {
-        date: "Somemonth 2021",
-        title: "Emergency Room Visit",
-        description:
-          "Admitted for chest pain; discharged after tests showed no major issues",
-      }, 
-      {
-        date: "Somemonth 2022",
-        title: "Annual Physical Exam",
-        description: "Routine checkup with primary care physician",
-      },
-
-      {
-        date: "Somemonth 2021",
-        title: "Emergency Room Visit",
-        description:
-          "Admitted for chest pain; discharged after tests showed no major issues",
-      }, 
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
+    {
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
+    {
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
+    {
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
+    {
+      date: "January 2022",
+      title: "Annual Physical Exam",
+      description: "Routine checkup with primary care physician",
+    },
+    
   ];
+
+  //To enable that only the authorised users can access these buttons we need to  check the patient is acutally the owner of th
+  const [showAllData, setShowAllData] = useState(false); //to rescrit the access of timeline
+  const toggleShowAllData = () => {
+    setShowAllData(!showAllData);
+  };
+
+
+  const [showRecentApprovals, setShowRecentApprovals] = useState(false); // to restrcit the access of recent approvals
+  const toggleShowRecentApprovals = () => {
+    if (patientAddress === authorizedAddress) {
+      setShowRecentApprovals(!showRecentApprovals);
+    }
+    else{
+      toast(`you are not authorised to access it only ${patientAddress} has the previlige`);
+    }
+  };
+
 
   return (
     <div className="Patient-body-container">
@@ -77,36 +84,62 @@ function PatientComponent() {
 
         {/* Timeline of the patient medical history */}
         <div className="timeline-container">
-          <div className="timeline-header"> Timeline of Health Activites </div>
+          <div className="timeline-header"> Timeline of Health Activities </div>
           <div className="timeline">
+
+
+            {/* Button to toggle showing authorized content */}
+             <div className="timeline-toggle-button">
+                <button onClick={toggleShowAllData}>
+                {showAllData ? "Hide TimeLine" : "Show TimeLine"}
+            </button>
+              </div> 
+
+
             {timelineData.map((event, index) => (
               <div key={index} className="timeline-event">
-                <div className="timeline-event-date">{event.date}</div>
-                <div className="timeline-event-details">
-                  <div className="timeline-event-title">
-                    <b>{event.title}</b>
+
+                 {showAllData || event.showOnlyToPatient ? ( 
+
+                  <div className="timeline-event">
+                    <div className="timeline-event-date">{event.date}</div>
+                    <div className="timeline-event-details">
+                      <div className="timeline-event-title">
+                        <b>{event.title}</b>
+                      </div>
+                      <div className="timeline-event-description">
+                        {event.description}
+                      </div>
+                    </div>
+                    {index !== timelineData.length - 1 && (
+                      <hr className="timeline-event-separator" />
+                    )}
                   </div>
-                  <div className="timeline-event-description">
-                    {event.description}
-                  </div>
-                </div>
-                {index !== timelineData.length - 1 && (
-                  <hr className="timeline-event-separator" />
-                )}
+
+                 ) : null} 
+
               </div>
             ))}
           </div>
         </div>
 
+
         {/* Profile of the patient */}
         <div className="profile-container">
           <div className="userdetails">
-          <div className="address-text">{patientAddress}
-          <div className="profile-icon">
-            <FontAwesomeIcon icon={faUserCircle} size="5x" />
+            <div className="address-text">{patientAddress}
+              <div className="profile-icon">
+                <FontAwesomeIcon icon={faUserCircle} size="5x" />
+              </div>
+               {/* Toggle for recent approvals */}
+          <div className="toggle-button">
+            <button onClick={toggleShowRecentApprovals}>
+              {showRecentApprovals ? "Hide Recent Approvals" : "Show Recent Approvals"}
+            </button>
           </div>
 
-          <div className="recent-approvals">
+            {showRecentApprovals && (
+            <div className="recent-approvals">
               <p>Recent Approvals:</p>
               <ul className="approved_addresses">
                 {recentApprovals.map((address, index) => (
@@ -114,7 +147,8 @@ function PatientComponent() {
                 ))}
               </ul>
             </div>
-          </div>
+          )}
+            </div>
           </div>       
         </div>
       </div>
